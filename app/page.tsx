32cb06727885fc43e350d56d21253275da6399f9
RotarAlexC <rotaralexc@gmail.com>
2024-12-21 01:30:16 +0200

    Implement Navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,6 @@
-/* eslint-disable tailwindcss/no-custom-classname */
 const Home = () => (
   <>
     <h1 className="h1-bold">Welcome to the world of Next.js </h1>
-    <h1 className="h1-bold font-space-grotesk">
-      Welcome to the world of Next.js
-    </h1>
   </>
 );
 
