c9c8e658235fd1120febdfae67fc2f7ce7b3136a
RotarAlexC <rotaralexc@gmail.com>
2024-12-21 03:02:43 +0200

    Set up Light & Dark Theme, Shadcn

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 
 import "./globals.css";
-import Navbar from "@/components/navigation";
+import Navbar from "@/components/navigation/navbar";
 import ThemeProvider from "@/context/Theme";
 
 const inter = localFont({
